a79c119f57e8a931365eafa9109f5c2d700b899d
Niko Kacic <niko.kacic@gmail.com>
2024-09-03 17:33:17 -0400

    Primer commit a las bases aprendidas de Angular, desde Componentes, Modulos, OWDB, directivas ng-if & ngIf-else

---
+++ 02-Bases/src/app/heroes/components/hero/hero.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age:  number = 45;
+
+  get capitalizedName():string {//get y set son PROPIEDADES que se comportan como metodos
+    return this.name.toLocaleUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero():void{
+    this.name = 'Spiderman';
+  }
+
+  changeAge():void{
+    this.age = 23;
+  }
+
+  resetForm():void{
+    this.name = 'iroman';
+    this.age = 45;
+  }
+
+}
