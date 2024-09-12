6b617be7784dcfdbe493fdd79cd47f146bdd6b30
Niko Kacic <niko.kacic@gmail.com>
2024-09-12 17:46:40 -0300

    Agregando funcionalidad eliminar personajes de la lista

--- 02-Bases/src/app/dbz/components/list/list.component.ts
+++ 02-Bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -12,6 +12,15 @@ export class ListComponent  {
     name: 'Trunks',
     power: 10
   }];
+
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index:number):void{
+    //TODO Emitir el ID del personaje
+    this.onDelete.emit(index);
+  }
 
 
 }
